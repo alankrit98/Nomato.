36909bf242f2479bbe7156ea03d442f4a4982e7f
ALANKRIT AGARWAL <134329921+alankrit98@users.noreply.github.com>
2026-03-25 12:41:14 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //placing user order for frontend
 const placeOrder = async (req, res) => {
 
-    const frontend_url = "https://food-delivery-full-stack-alankrit-agarwals-projects.vercel.app";
+    const frontend_url = "https://food-delivery-full-stack-pi.vercel.app";
 
     try {
         const newOrder = new orderModel({
